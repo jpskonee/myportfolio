20c4cb2be3715c4f309338dc4d4b9dfde2b9b3ab
AGHO-DEV <aghoemmanuel.steam@gmail.com>
2021-06-13 21:33:35 +0100

    first commit

--- src/components/Skills/SkillsTab.js
+++ src/components/Skills/SkillsTab.js
@@ -8,9 +8,7 @@ import CodeIcon from "@material-ui/icons/Code";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import StorageIcon from "@material-ui/icons/Storage";
 import VpnKeyIcon from "@material-ui/icons/VpnKey";
-import ThumbDown from "@material-ui/icons/ThumbDown";
 import PaletteIcon from "@material-ui/icons/Palette";
-import BuildIcon from "@material-ui/icons/Build";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 
@@ -49,10 +47,10 @@ function a11yProps(index) {
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
     width: "100%",
+    flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(3),
   },
   rootDiv: {},
 }));
@@ -71,19 +69,17 @@ const SkillsTab = () => {
         <Tabs
           value={value}
           onChange={handleChange}
-          variant="scrollable"
           scrollButtons="on"
           indicatorColor="primary"
           textColor="primary"
           aria-label="scrollable force tabs example"
+          centered
         >
           <Tab label="Front-End" icon={<PaletteIcon />} {...a11yProps(0)} />
           <Tab label="Back-End" icon={<CodeIcon />} {...a11yProps(1)} />
           <Tab label="C.M.S" icon={<ShoppingCartIcon />} {...a11yProps(2)} />
           <Tab label="Database" icon={<StorageIcon />} {...a11yProps(3)} />
           <Tab label="Web Dev" icon={<VpnKeyIcon />} {...a11yProps(4)} />
-          <Tab label="STEAM" icon={<BuildIcon />} {...a11yProps(5)} />
-          <Tab label="STEAM" icon={<ThumbDown />} {...a11yProps(6)} />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
@@ -100,12 +96,6 @@ const SkillsTab = () => {
       </TabPanel>
       <TabPanel value={value} index={4}>
         Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
       </TabPanel>
     </div>
   );
